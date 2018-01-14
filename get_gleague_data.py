ef7ff89360790344b1190e39eb4df46c73035a65
Cole Fisher <cfishe29@uwo.ca>
2018-01-14 14:58:08 -0500

    reload script

--- get_gleague_data.py
+++ get_gleague_data.py
@@ -38,3 +38,18 @@ for obj in data:
 players_file = open('GLeaguePredictions/GLeague-Predictions/data/players_json.txt', 'w')
 players_file.write(str(player_json))
 players_file.close()
+
+my_domain = 'www.gleaguetonba.com'
+username = 'cfisher5'
+token = 'a844de4d08ca51a979b47c54e80acea23a434b8e'
+
+response = requests.post(
+    'https://www.pythonanywhere.com/api/v0/user/{username}/webapps/{domain}/reload/'.format(
+        username=username, domain=my_domain
+    ),
+    headers={'Authorization': 'Token {token}'.format(token=token)}
+)
+if response.status_code == 200:
+    print('All OK')
+else:
+    print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
