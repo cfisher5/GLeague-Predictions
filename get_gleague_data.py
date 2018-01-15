b1526c23088b9c0d85addb40df8549c744c0b130
Cole Fisher <cfishe29@uwo.ca>
2018-01-15 12:39:15 -0500

    fixed scraper script

--- get_gleague_data.py
+++ get_gleague_data.py
@@ -1,15 +1,35 @@
 import requests
 import os
+import json
 
 player_json = []
-print(os.getcwd())
+wd = os.getcwd()
+print(wd)
 
-gleague_ids = open('GLeaguePredictions/GLeague-Predictions/data/gleague_data.csv', 'w')
+if wd == "/home/cfisher5":
+    csv_filename = "GLeaguePredictions/GLeague-Predictions/data/gleague_data_new.csv"
+    json_filename = "GLeaguePredictions/GLeague-Predictions/data/players_json_new.txt"
+    old_csv_filename = "GLeaguePredictions/GLeague-Predictions/data/gleague_data.csv"
+    old_json_filename = "GLeaguePredictions/GLeague-Predictions/data/players_json.txt"
+else:
+    csv_filename = "data/gleague_data_new.csv"
+    json_filename = "data/players_json_new.txt"
+    old_csv_filename = "data/gleague_data.csv"
+    old_json_filename = "data/players_json.txt"
+
+gleague_ids = open(csv_filename, 'w')
 gleague_ids.write('Name,ID,Team,Age,GP,FGA,3PM,REB,AST,PTS\n')
 
 url = "http://stats.gleague.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=20&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2017-18&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight="
-response = requests.get(url)
-data = response.json()['resultSets'][0]['rowSet']
+
+try:
+    response = requests.get(url)
+    data = response.json()['resultSets'][0]['rowSet']
+except json.JSONDecodeError:
+    print("unable to reach API")
+    os.remove(csv_filename)
+    exit(1)
+
 for obj in data:
     player = {}
     name = obj[1]
@@ -35,7 +55,7 @@ for obj in data:
 
     gleague_ids.write(name + "," + href + "," + team + "," + age + "," + gp + "," + fga + "," + threepm + "," + reb + "," + ast + "," + pts + "\n")
 
-players_file = open('GLeaguePredictions/GLeague-Predictions/data/players_json.txt', 'w')
+players_file = open(json_filename, 'w')
 players_file.write(str(player_json))
 players_file.close()
 
@@ -53,3 +73,10 @@ if response.status_code == 200:
     print('All OK')
 else:
     print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
+
+
+os.remove(old_csv_filename)
+os.remove(old_json_filename)
+
+os.rename(csv_filename, old_csv_filename)
+os.rename(json_filename, old_json_filename)
