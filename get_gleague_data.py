aa3e88d6df9e008c3f5b781e1a129cd4421d3a55
Cole Fisher <cfishe29@uwo.ca>
2018-04-07 02:36:16 -0700

    fixing scripts

--- get_gleague_data.py
+++ get_gleague_data.py
@@ -147,21 +147,6 @@ def scrape():
     pace_file.write("avg," + str(avg_pace))
     pace_file.close()
 
-    my_domain = 'www.gleaguetonba.com'
-    username = 'cfisher5'
-    token = 'a844de4d08ca51a979b47c54e80acea23a434b8e'
-
-    response = requests.post(
-        'https://www.pythonanywhere.com/api/v0/user/{username}/webapps/{domain}/reload/'.format(
-            username=username, domain=my_domain
-        ),
-        headers={'Authorization': 'Token {token}'.format(token=token)}, timeout=10
-    )
-    if response.status_code == 200:
-        print('All OK')
-    else:
-        print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
-
     os.remove(old_csv_filename)
     os.remove(old_json_filename)
 
--- get_nba_player_info.py
+++ get_nba_player_info.py
@@ -3,14 +3,23 @@ import os
 import json
 import pandas as pd
 
+
+wd = os.getcwd()
+
+if wd == "/home/cfisher5":
+    csv_filename = "GLeaguePredictions/GLeague-Predictions/data/nba_player_data.csv"
+    per_36 = "GLeaguePredictions/GLeague-Predictions/data/nba36.csv"
+    merge_filename = "GLeaguePredictions/GLeague-Predictions/data/nba_merge.csv"
+    nba_data = "GLeaguePredictions/GLeague-Predictions/data/nba_player_data.csv"
+
+else:
+    csv_filename = "data/nba_player_data.csv"
+    per_36 = "data/nba36.csv"
+    merge_filename = "data/nba_merge.csv"
+    nba_data = "data/nba_player_data.csv"
+
+
 def scrape():
-    wd = os.getcwd()
-
-    if wd == "/home/cfisher5":
-        csv_filename = "GLeaguePredictions/GLeague-Predictions/data/nba_player_data.csv"
-
-    else:
-        csv_filename = "data/nba_player_data.csv"
 
     nba_data = open(csv_filename, 'w')
     nba_data.write('ID,height,height_inches,weight,pos\n')
@@ -40,14 +49,31 @@ def scrape():
     nba_data.close()
     return True
 
+
 done = False
 while not done:
+
     done = scrape()
-    data = pd.read_csv("data/nba36.csv", sep=",")
+    data = pd.read_csv(per_36, sep=",")
     data = data[['ID', 'PTS', 'REB', 'AST', 'STL', 'BLK', 'FGper', 'threeper', 'FTper']]
-    height_weight_data = pd.read_csv("data/nba_player_data.csv", sep=",")
+    height_weight_data = pd.read_csv(nba_data, sep=",")
     height_weight_data = height_weight_data[['ID', 'height_inches', 'weight']]
 
     merge = pd.merge(left=data, right=height_weight_data, how='inner', on='ID')
-    merge.to_csv("data/nba_merge.csv")
+    merge.to_csv(merge_filename)
     merged = merge.drop('ID', axis=1)
+
+my_domain = 'www.gleaguetonba.com'
+username = 'cfisher5'
+token = 'a844de4d08ca51a979b47c54e80acea23a434b8e'
+
+response = requests.post(
+    'https://www.pythonanywhere.com/api/v0/user/{username}/webapps/{domain}/reload/'.format(
+        username=username, domain=my_domain
+    ),
+    headers={'Authorization': 'Token {token}'.format(token=token)}, timeout=10
+)
+if response.status_code == 200:
+    print('All OK')
+else:
+    print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
